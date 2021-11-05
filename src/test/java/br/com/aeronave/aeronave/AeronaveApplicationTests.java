8b7fa32ab3ac847c8271596b540188f5b2ac9114
Rúben Filipe Andrade de Oliveira <rubenfilipeao@gmail.com>
2021-11-04 23:03:34 -0300

    Commit inicial

---
+++ src/test/java/br/com/aeronave/aeronave/AeronaveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aeronave.aeronave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeronaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
