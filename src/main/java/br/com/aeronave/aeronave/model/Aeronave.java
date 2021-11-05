7441a2da807dea30a25f55ed5d6287fe4fc0df32
Rúben Filipe Andrade de Oliveira <rubenfilipeao@gmail.com>
2021-11-04 23:13:43 -0300

    Criado o model da Aeronave

---
+++ src/main/java/br/com/aeronave/aeronave/model/Aeronave.java
@@ -0,0 +1,50 @@
+package br.com.aeronave.aeronave.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Aeronave {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable = false)
+	private String nome;
+	@Column(nullable = false)
+	private String marca;
+	@Column(nullable = false)
+	private Integer ano;
+	@Column(nullable = false)
+	private String descricao;
+	@Column(nullable = false)
+	private boolean vendido;
+	@Column(nullable = false)
+	private LocalDateTime created;
+	private LocalDateTime updated;
+
+	@Override
+	public String toString() {
+		return "Aeronave [id=" + id + ", nome=" + nome + ", marca=" + marca + ", ano=" + ano + ", descricao="
+				+ descricao + ", vendido=" + vendido + ", created=" 
+				+ (created == null? "null" : created.toLocalDate() + " " + created.toLocalTime()) // Ternary operator
+				+ ", updated=" 
+				+ (updated == null? "null" : updated.toLocalDate() + " " + updated.toLocalTime()) // Ternary operator
+				+ "]";
+	}
+}
---
+++ src/main/java/br/com/aeronave/aeronave/model/EnumMarca.java
@@ -0,0 +1,5 @@
+package br.com.aeronave.aeronave.model;
+
+public enum EnumMarca {
+	BOMBARDIER, BOEING, AIRBUS, EMBRAER, GE_AVIATION
+}
